05ffc4380032f3ae252a3cc38a83c23bccdaa6c3
roberto-pf <roberto.pf3@gmail.com>
2021-12-11 00:59:25 -0800

    Init Version Example 1

---
+++ example1-gitactions/src/main/java/es/com/disastercode/exampe/gitactions/examplegitactions/ExampleGitactionsApplication.java
@@ -0,0 +1,13 @@
+package es.com.disastercode.exampe.gitactions.examplegitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitactionsApplication.class, args);
+	}
+
+}
---
+++ example1-gitactions/src/test/java/es/com/disastercode/exampe/gitactions/examplegitactions/ExampleGitactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.com.disastercode.exampe.gitactions.examplegitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
